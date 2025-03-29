b6b9318c4d030a98032dbae8da735fa1390f303e
Melomaner <>
2025-03-29 21:36:46 +0300

    Лодка, локация, дома

---
+++ Assets/Scripts/Locations/LocationSpawnItem.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[System.Serializable]
+public class LocationSpawnItem
+{
+    public GameObject prefab; // Префаб объекта
+    public float chance;      // Вес (шанс) появления данного объекта
+}
--- Assets/Scripts/Train/TrainMagnet.cs
+++ Assets/Scripts/Train/TrainMagnet.cs
@@ -4,12 +4,12 @@ public class TrainMagnet : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag != "GravityPlatform")
+        if (other.tag == "Player" || other.tag == "Enemy")
         other.transform.SetParent(this.gameObject.transform.parent);
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag != "GravityPlatform")
+        if (other.tag == "Player" || other.tag == "Enemy")
             other.transform.SetParent(null);
     }
 }
